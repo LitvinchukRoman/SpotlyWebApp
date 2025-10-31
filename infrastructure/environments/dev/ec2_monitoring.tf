0d91dedab2fc4692d40411dbaf8c53b4226e242f
litvi <litvin4uk.roman@gmail.com>
2025-10-31 16:24:51 +0200

    Monitoring EC2

---
+++ infrastructure/environments/dev/ec2_monitoring.tf
@@ -0,0 +1,23 @@
+module "monitoring_ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "spotly-monitoring"
+
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deployer_key.key_name
+  monitoring             = true
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  create_eip             = true
+  create_security_group  = false
+
+  ami = data.aws_ami.ubuntu.id
+
+  user_data = file("${path.module}/setup_script.sh")
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+}
