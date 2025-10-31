049f472245b695d85557d7729fcdade8cab97352
litvi <litvin4uk.roman@gmail.com>
2025-10-31 17:14:52 +0200

    Security group 9100 ports opened

--- infrastructure/environments/dev/security_group.tf
+++ infrastructure/environments/dev/security_group.tf
@@ -103,6 +103,29 @@ resource "aws_security_group" "monitoring_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  ingress {
     description = "Grafana"
     from_port   = 3000
     to_port     = 3000
