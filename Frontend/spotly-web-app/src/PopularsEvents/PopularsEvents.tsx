99e58b45e6f434ca3ba1ae4a695a61f7634e6e16
Pavlo <oksanakopalko@gmail.com>
2025-11-02 03:46:30 +0200

    add task solution

--- Frontend/spotly-web-app/src/PopularsEvents/PopularsEvents.tsx
+++ Frontend/spotly-web-app/src/PopularsEvents/PopularsEvents.tsx
@@ -17,22 +17,22 @@ const PopularsEvents = () => {
 
         <div className={cn(eventsStyle.popEvents__firstCategory, eventsStyle.popEvents__categories)}>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/startaps.png" alt="image" />
+            <img src="./images/startaps.png" alt="image" />
             <h3>Pitch Night: Стартапи в дії</h3>
             <div>Launch Point Ukraine</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/motherHoodCouncil.png" alt="image" />
+            <img src="./images/motherHoodCouncil.png" alt="image" />
             <h3>Чесна розмова про материнство: без порад, тіль...</h3>
             <div>МамКом’юніті «Ми тут»</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/bookEvents.png" alt="image" />
+            <img src="./images/bookEvents.png" alt="image" />
             <h3>Читаємо українське: Забужко vs Жадан</h3>
             <div>UkrBook Movement</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/saturdays.png" alt="image" />
+            <img src="./images/saturdays.png" alt="image" />
             <h3>Суботник на Пейзажці</h3>
             <div>Еко-ініціатива «Green Kyiv»</div>
           </div>
@@ -49,22 +49,22 @@ const PopularsEvents = () => {
 
         <div className={cn(eventsStyle.popEvents__secondCategory, eventsStyle.popEvents__categories)}>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/picnic.png" alt="image" />
+            <img src="./images/picnic.png" alt="image" />
             <h3>Пікнік на заході сонця біля Дніпра</h3>
             <div>Яна Ярмоленко</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/tableGames.png" alt="image" />
+            <img src="./images/tableGames.png" alt="image" />
             <h3>Настолки та пиво в М’ясторії</h3>
             <div>Дмитро Навроцький</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/adventures.png" alt="image" />
+            <img src="./images/adventures.png" alt="image" />
             <h3>Маленька подорож у Карпати: Синевир на 2 дні</h3>
             <div>Костянтин Задорожній</div>
           </div>
           <div className={eventsStyle.popEvents__singleEvent}>
-            <img src="../../public/images/films.png" alt="image" />
+            <img src="./images/films.png" alt="image" />
             <h3>Кіновечір на березі Дніпра: Barbie vs Oppenheimer</h3>
             <div>КіноКлуб «Frame by Frame»</div>
           </div>
