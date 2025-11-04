8c7a7d0fd237e37d4f525813b5a32408529ee7f1
Artem <artemgritsenko2007@gmail.com>
2025-11-05 01:49:21 +0200

    feat(security): Implement Google OAuth2 login flow

--- backend/src/main/java/com/spotly/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/spotly/backend/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -28,14 +26,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -51,5 +45,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
--- backend/src/main/java/com/spotly/backend/security/CustomOAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/spotly/backend/security/CustomOAuth2LoginSuccessHandler.java
@@ -63,6 +63,7 @@ public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHan
                 .queryParam("token", ourJwtToken)
                 .build().toUriString();
 
+
         response.sendRedirect(targetUrl);
     }
 }
