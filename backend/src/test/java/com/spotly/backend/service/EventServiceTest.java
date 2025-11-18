d526055193a03bc169508d17f78d6534ab285bdf
Artem <artemgritsenko2007@gmail.com>
2025-11-18 11:15:06 +0200

    test(service): Add unit tests for EventService logic and configure Mockito
    - Cover createEvent, getEventById, deleteEvent, and getRecommendedEvents with tests - Add 'mock-maker-subclass' resource to resolve JDK 21 warnings - Verify interaction with UserRepository, EventRepository, and CategoryRepository

--- backend/src/test/java/com/spotly/backend/service/EventServiceTest.java
+++ backend/src/test/java/com/spotly/backend/service/EventServiceTest.java
@@ -1,8 +1,11 @@
 package com.spotly.backend.service;
 
+import com.spotly.backend.domain.Category;
 import com.spotly.backend.domain.Event;
 import com.spotly.backend.domain.User;
+import com.spotly.backend.dto.CreateEventDto;
 import com.spotly.backend.dto.EventDto;
+import com.spotly.backend.exception.AccessDeniedException;
 import com.spotly.backend.repository.CategoryRepository;
 import com.spotly.backend.repository.EventRepository;
 import com.spotly.backend.repository.UserRepository;
@@ -17,11 +20,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 class EventServiceTest {
@@ -37,7 +44,7 @@ class EventServiceTest {
     private EventService eventService;
 
     @Test
-    void testGetAllEvents_ShouldReturnPageOfEventDtos() {
+    void shouldReturnAllEventsWhenEventsExist() {
 
         User fakeAuthor = new User();
         fakeAuthor.setFirstName("Тест");
@@ -68,14 +75,14 @@ class EventServiceTest {
 
         Assertions.assertNotNull(resultPage);
         Assertions.assertEquals(1, resultPage.getTotalElements());
-        Assertions.assertEquals("Тестова Подія", resultPage.getContent().get(0).title());
+        Assertions.assertEquals("Тестова Подія", resultPage.getContent().getFirst().title());
 
         Mockito.verify(eventRepository).findAll(pageable);
     }
 
 
     @Test
-    void testGetEventById_ShouldReturnEvent_WhenFound() {
+    void shouldReturnEventWhenGetByIdAndFound() {
 
         Long eventId = 1L;
         User fakeAuthor = new User();
@@ -102,4 +109,172 @@ class EventServiceTest {
         Assertions.assertEquals("Знайдена Подія", resultDto.title());
         Assertions.assertEquals(1L, resultDto.id());
     }
+
+    @Test
+    void shouldDeleteEventWhenUserIsAuthor() {
+        User fakeAuthor = new User();
+        fakeAuthor.setId(1L);
+        fakeAuthor.setEmail("test@example.com");
+
+        Event fakeEvent = new Event();
+        fakeEvent.setId(1L);
+        fakeEvent.setAuthor(fakeAuthor);
+
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        Mockito.when(authentication.getName()).thenReturn("test@example.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        Mockito.when(eventRepository.findById(1L))
+                .thenReturn(Optional.of(fakeEvent));
+
+        eventService.deleteEvent(1L);
+
+        Mockito.verify(eventRepository, Mockito.times(1)).delete(fakeEvent);
+    }
+
+    @Test
+    void shouldThrowAccessDeniedWhenUserIsNotAuthor(){
+        User authorUser = new User();
+        authorUser.setId(1L);
+        authorUser.setEmail("author@example.com");
+        authorUser.setFirstName("Автор");
+        authorUser.setLastName("Події");
+
+        User attackerUser = new User();
+        attackerUser.setId(2L);
+        attackerUser.setEmail("hacker@example.com");
+        attackerUser.setFirstName("Злий");
+        attackerUser.setLastName("Хакер");
+
+        Event fakeEvent = new Event();
+        fakeEvent.setId(10L);
+        fakeEvent.setAuthor(authorUser);
+
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        Mockito.when(authentication.getName()).thenReturn("hacker@example.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        Mockito.when(eventRepository.findById(10L)).thenReturn(Optional.of(fakeEvent));
+
+        Assertions.assertThrows(AccessDeniedException.class, () -> eventService.deleteEvent(10L));
+
+        Mockito.verify(eventRepository, Mockito.never()).delete(fakeEvent);
+    }
+
+    @Test
+    void shouldSaveAndReturnDtoWhenCreateEvent(){
+
+        Set<Long> categoryIds = Set.of(1L, 2L);
+        CreateEventDto createDto = new CreateEventDto(
+                "Нова Подія",
+                "Опис події",
+                "Київ",
+                50.0,
+                30.0,
+                categoryIds,
+                "Вул. Хрещатик",
+                0.0,
+                "2025-01-01T10:00:00",
+                "2025-01-01T12:00:00",
+                100,
+                "http://example.com/image.jpg"
+        );
+
+        User fakeAuthor = new User();
+        fakeAuthor.setId(10L);
+        fakeAuthor.setEmail("author@test.com");
+        fakeAuthor.setFirstName("Test");
+        fakeAuthor.setLastName("User");
+
+        Category fakeCategory = new Category();
+        fakeCategory.setId(1L);
+        fakeCategory.setName("Музика");
+
+        Event savedEvent = new Event();
+        savedEvent.setId(1L);
+        savedEvent.setTitle(createDto.title());
+        savedEvent.setAuthor(fakeAuthor);
+        savedEvent.setCategories(Set.of(fakeCategory));
+        savedEvent.setStartDateTime(LocalDateTime.parse(createDto.startDateTime()));
+        savedEvent.setEndDateTime(LocalDateTime.parse(createDto.endDateTime()));
+
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        Mockito.when(authentication.getName()).thenReturn("author@test.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        Mockito.when(userRepository.findByEmail("author@test.com"))
+                .thenReturn(Optional.of(fakeAuthor));
+
+        Mockito.when(categoryRepository.findAllById(categoryIds))
+                .thenReturn(List.of(fakeCategory));
+        Mockito.when(eventRepository.save(Mockito.any(Event.class)))
+                .thenReturn(savedEvent);
+
+        EventDto resultDto = eventService.createEvent(createDto);
+
+        Assertions.assertNotNull(resultDto);
+        Assertions.assertEquals(1L, resultDto.id());
+        Assertions.assertEquals("Нова Подія", resultDto.title());
+        Assertions.assertEquals("Test User", resultDto.organisatorName());
+
+        Mockito.verify(eventRepository, Mockito.times(1)).save(Mockito.any(Event.class));
+    }
+
+    @Test
+    void shouldReturnListWhenUserHasInterests(){
+        Category musicCategory = new Category();
+        musicCategory.setId(1L);
+        musicCategory.setName("Музика");
+
+        User currentUser = new User();
+        currentUser.setEmail("fan@test.com");
+        currentUser.setFirstName("Fan");
+        currentUser.setLastName("User");
+        currentUser.setInterestedCategories(Set.of(musicCategory));
+
+        User author = new User();
+        author.setFirstName("Author");
+        author.setLastName("Name");
+
+        Event recommendedEvent = new Event();
+        recommendedEvent.setId(10L);
+        recommendedEvent.setTitle("Концерт");
+        recommendedEvent.setAuthor(author);
+        recommendedEvent.setCategories(Set.of(musicCategory));
+        recommendedEvent.setStartDateTime(LocalDateTime.now());
+        recommendedEvent.setEndDateTime(LocalDateTime.now().plusHours(2));
+        recommendedEvent.setAddress("Test Address");
+        recommendedEvent.setPrice(50.0);
+
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        Mockito.when(authentication.getName()).thenReturn("fan@test.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        Mockito.when(userRepository.findByEmail("fan@test.com"))
+                .thenReturn(Optional.of(currentUser));
+
+        Mockito.when(eventRepository.findDistinctByCategoriesIn(currentUser.getInterestedCategories(), Pageable.ofSize(10)))
+                .thenReturn(new PageImpl<>(List.of(recommendedEvent)));
+
+        Page<EventDto> resultPage = eventService.getRecommendedEvents(Pageable.ofSize(10));
+
+        Assertions.assertNotNull(resultPage);
+        Assertions.assertEquals(1, resultPage.getTotalElements());
+        Assertions.assertEquals("Концерт", resultPage.getContent().getFirst().title());
+
+        Mockito.verify(eventRepository, Mockito.times(1))
+                .findDistinctByCategoriesIn(currentUser.getInterestedCategories(), Pageable.ofSize(10));
+
+    }
+
+
+
 }
